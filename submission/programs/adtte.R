71b2d6ab0311efa468c495fd102b7a152196e6aa
Ben Straub <ben.x.straub@gsk.com>
2023-02-24 18:28:32 +0000

    updated to admiral code

--- submission/programs/adtte.R
+++ submission/programs/adtte.R
@@ -47,12 +47,21 @@ adtte_spec <- metacore %>%
   select_dataset("ADTTE")
 
 
-event <- adae %>%
-  filter(AOCC01FL == "Y", CQ01NAM == "DERMATOLOGIC EVENTS", SAFFL =="Y") %>%
-  select(STUDYID, USUBJID, AESEQ, AOCC01FL, CQ01NAM, ASTDT)
+event <- event_source(
+  dataset_name = "adae",
+  filter = AOCC01FL == "Y" & CQ01NAM == "DERMATOLOGIC EVENTS" & SAFFL =="Y",
+  date = ASTDT,
+  set_values_to = vars(
+    EVNTDESC = "Dematologic Event Occured",
+    SRCDOM = "ADAE",
+    SRCVAR = "ASTDT",
+    SRCSEQ = AESEQ
+  )
+)
 
 
 # Censor events --------------------------------------------------------- 
+
 ## discontinuation, completed, death
 ds00 <- ds %>%
   select(STUDYID, USUBJID, DSCAT, DSDECOD, DSSTDTC) %>%
@@ -68,56 +77,54 @@ adsl <- adsl %>%
     by_vars = vars(STUDYID, USUBJID),
     new_vars = vars(EOSDT = DSSTDT),
     filter_add = DSCAT == "DISPOSITION EVENT" & DSDECOD != "SCREEN FAILURE" & DSDECOD != "FINAL LAB VISIT"
-  )
-
-censor <- adsl %>%
-  filter(SAFFL == "Y") %>%
+  ) %>%
   # Analysis uses DEATH date rather than discontinuation when subject dies even if discontinuation occurs before death
   # Observed through QC - However not described in specs
   mutate(EOS2DT = case_when(DCDECOD == "DEATH" ~ as.Date(RFENDTC),
                                    DCDECOD != "DEATH" ~ EOSDT)
-                )%>%
-  select(STUDYID, SITEID, USUBJID, AGE, AGEGR1, AGEGR1N, RACE, RACEN,
-                SEX, TRTSDT, TRTEDT, TRTDURD, EOSDT, EOS2DT, EOSSTT, SAFFL,
-                TRT01P, TRT01PN, TRT01A, TRT01AN) %>%
-  rename(TRTDUR = TRTDURD, # total duration is same as in days
-                TRTPN = TRT01PN,
-                TRTP = TRT01P,
+  )
+
+censor <- censor_source(
+  dataset_name = "adsl",
+  date = EOS2DT,
+  set_values_to = vars(
+    EVNTDESC = "Study Completion Date",
+    SRCDOM = "ADSL",
+    SRCVAR = "RFENDT"
+  )
+)
+
+
+adtte_pre <- derive_param_tte(
+  dataset_adsl = adsl,
+  start_date = TRTSDT,
+  event_conditions = list(event),
+  censor_conditions = list(censor),
+  source_datasets = list(adsl = adsl, adae = adae),
+  set_values_to = vars(PARAMCD = "TTDE", PARAM = "Time to First Dermatologic Event")
+) %>% 
+  derive_vars_duration(
+    new_var = AVAL,
+    start_date = STARTDT,
+    end_date = ADT
+  ) %>% 
+  derive_vars_merged(
+    dataset_add = adsl,
+    new_vars = vars(AGE, AGEGR1, AGEGR1N, RACE, RACEN, SAFFL, SEX, SITEID, TRT01A,
+                    TRT01AN, TRTDURD, TRTEDT, TRT01P, TRTSDT),
+    by_vars = vars(STUDYID, USUBJID)
+  ) %>% 
+  rename(TRTA = TRT01A,
                 TRTAN = TRT01AN,
-                TRTA = TRT01A) 
-
-# Combine event and censor ---------------------------------------------- 
-
-adtte_pre <- event %>%
-  right_join(censor, by = c("STUDYID", "USUBJID")) %>%
-  mutate(PARAM = "Time to First Dermatologic Event",
-                PARAMCD = "TTDE",
-                STARTDT = TRTSDT,
-                #pmin = vectorized solution required as min took the minimum across both vectors
-                ADT = pmin(EOS2DT, ASTDT, na.rm = TRUE), 
-                AVAL = as.numeric(ADT-STARTDT+1),
-                CNSR = ifelse(!is.na(ASTDT) & ADT == ASTDT, 0, 1),
-                # NA were causing issues hence the long date comparison expressions
-                # Study discontinuation is also seen as study completion
-                EVNTDESC = case_when((is.na(ASTDT) | (!is.na(ASTDT) & ASTDT != ADT) ) & EOSSTT == "DEATH" ~ "Death Date",
-                                     (is.na(ASTDT) | (!is.na(ASTDT) & ASTDT != ADT) ) & EOSSTT %in% c("COMPLETED", "DISCONTINUED") ~ "Study Completion Date",
-                                     (is.na(ASTDT) | (!is.na(ASTDT) & ASTDT != ADT) ) & EOSSTT == "DISCONTINUED" ~ "Study Discontinuation Date",
-                                     !is.na(ASTDT) & (ADT == ASTDT) ~ "Dematologic Event Occured"),
-                SRCDOM = case_when(EVNTDESC ==  "Dematologic Event Occured" ~ "ADAE",
-                                   TRUE ~ "ADSL"),
-                SRCVAR = case_when(EVNTDESC == "Death Date" ~ "DTHDT",
-                                   EVNTDESC == "Study Completion Date" ~ "RFENDT",
-                                   EVNTDESC ==  "Dematologic Event Occured" ~ "ASTDT"),
-                SRCSEQ = case_when(!is.na(ASTDT) & (ADT == ASTDT) ~ as.numeric(AESEQ))
-                ) 
-
+                TRTDUR = TRTDURD,
+                TRTP = TRT01P)
 
 adtte <- adtte_pre %>% 
   drop_unspec_vars(adtte_spec) %>% # only keep vars from define
   order_cols(adtte_spec) %>% # order columns based on define
   set_variable_labels(adtte_spec) %>% # apply variable labels based on define
   # xportr_type(adtte_spec, "ADTTE") %>%
-  #xportr_length(adtte_spec, "ADTTE") %>%
+  # xportr_length(adtte_spec, "ADTTE") %>%
   # unresolved issue in xportr_length due to:
   # https://github.com/tidyverse/haven/issues/699
   # no difference found by diffdf after commenting out xportr_length()
@@ -127,4 +134,10 @@ adtte <- adtte_pre %>%
                label = "AE Time To 1st Derm. Event Analysis"
   )
 
+prod <- read_xpt("adam/adtte.xpt")
+
+a <- diffdf(prod, adtte, keys = c("STUDYID", "USUBJID"))
+
+a$VarClassDiffs
+a$AttribDiffs
 # END of Code -------------------------------------------------------------
